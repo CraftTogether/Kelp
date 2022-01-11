de709ff08cd121fa770383b94817994dd2dd5ba2
Marian <elias-marian@web.de>
2022-01-11 09:53:16 +0100

    :tada: Added basic JDA instance and event registration

---
+++ src/main/java/com/github/crafttogether/kelp/Kelp.java
@@ -0,0 +1,53 @@
+package com.github.crafttogether.kelp;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import javax.security.auth.login.LoginException;
+import java.util.function.Consumer;
+
+public class Kelp extends ListenerAdapter {
+
+    private static Kelp INSTANCE;
+    public static Boolean connected = false;
+    public static JDA jda;
+    private static Consumer<ReadyEvent> readyConsumer;
+
+    public static Kelp getInstance() {
+        if (INSTANCE == null) INSTANCE = new Kelp();
+        return INSTANCE;
+    }
+
+    public void connect() throws LoginException, InterruptedException {
+        jda = JDABuilder.createLight(Plugin.get("token"))
+                .addEventListeners(INSTANCE)
+                .build()
+                .awaitReady();
+        connected = true;
+    }
+
+    public void disconnect() {
+        if (jda == null) return;
+
+        jda.shutdown();
+    }
+
+    public void onReady(@NotNull ReadyEvent event) {
+        readyConsumer.accept(event);
+    }
+
+    public static void onReady(Consumer<ReadyEvent> consumer) {
+        readyConsumer = consumer;
+    }
+
+    public void addListeners(EventListener... eventListeners) {
+        for (EventListener listener : eventListeners) {
+            jda.addEventListener(listener);
+        }
+    }
+
+}
---
+++ src/main/java/com/github/crafttogether/kelp/Plugin.java
@@ -0,0 +1,43 @@
+package com.github.crafttogether.kelp;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.security.auth.login.LoginException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public final class Plugin extends JavaPlugin {
+
+    public static JavaPlugin plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+        try {
+            getConfig().load(Files.newBufferedReader(Path.of(getDataFolder() + "/config.yml")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Kelp.getInstance().connect();
+        } catch (LoginException | InterruptedException e) {
+            Bukkit.getLogger().warning("Failed to connect to discord");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        Kelp.getInstance().disconnect();
+    }
+
+    public static String get(String key) {
+        return plugin.getConfig().getString(key);
+    }
+
+}
