33928ffb634b5e8e7bb861df7e678f2af7d5bcd4
Marian <elias-marian@web.de>
2022-01-24 11:18:50 +0100

    :bug: Fixed registering listeners before jda is initialized

--- src/main/java/com/github/crafttogether/kelp/Kelp.java
+++ src/main/java/com/github/crafttogether/kelp/Kelp.java
@@ -3,17 +3,21 @@ package com.github.crafttogether.kelp;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.ReadyEvent;
-import net.dv8tion.jda.api.hooks.EventListener;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class Kelp extends ListenerAdapter {
 
     private static Kelp INSTANCE;
     private static Boolean connected = false;
+    private static List<ListenerAdapter> listeners = new ArrayList<>();
     private static JDA jda;
     private static Consumer<ReadyEvent> readyConsumer = null;
 
@@ -32,7 +36,7 @@ public class Kelp extends ListenerAdapter {
 
     public void connect() throws LoginException, InterruptedException {
         jda = JDABuilder.createLight(Plugin.getPlugin().getConfig().getString("token"))
-                .addEventListeners(INSTANCE)
+                .addEventListeners(INSTANCE, listeners)
                 .build()
                 .awaitReady();
         connected = true;
@@ -52,10 +56,11 @@ public class Kelp extends ListenerAdapter {
         readyConsumer = consumer;
     }
 
-    public static void addListeners(EventListener... eventListeners) {
-        for (EventListener listener : eventListeners) {
-            jda.addEventListener(listener);
+    public static void addListeners(ListenerAdapter... eventListeners) {
+        if (jda != null) {
+            jda.addEventListener(Arrays.stream(eventListeners).toList());
         }
+        Collections.addAll(listeners, eventListeners);
     }
 
 }
